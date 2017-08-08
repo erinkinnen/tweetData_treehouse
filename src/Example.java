091babf4588bed4b5c818977fb718bc7028c9f37
Erin Kinnen <erin.kinnen@outlook.com>
2017-08-07 20:00:04 -0500

    Creates class and object. Exercise includes immutability

--- src/Example.java
+++ src/Example.java
@@ -1,9 +1,14 @@
+import java.util.Date;
 import com.teamtreehouse.Treet;
 
 public class Example {
 
     public static void main(String[] args) {
-        Treet treet = new Treet();
+        Treet treet = new Treet(
+          "erinkinnen",
+          "1st treet",
+           new Date(1502153818L)
+        );
         System.out.printf("This is a new Treet: %s %n", treet);
     }
 
